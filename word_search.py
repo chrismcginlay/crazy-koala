356c7259957e453342d2a9139ff7893c487dabd2
chrismcginlay <ctmcginlay@gmail.com>
2016-06-28 21:58:06 +0100

    Rudimentary, wors away from edges

--- word_search.py
+++ word_search.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 28 09:55:21 2016
+
+@author: chrismcginlay
+"""
+
 grid = [
     list("SYNTAXQWERT"),
     list("GHFPOSTKDSK"),
     list("LKJHCVNBVYR"),
     list("CCCBIWUISKT"),
-    list("LKJVMDSHDER"),
-    list("XZKGEDSEIGP"),
+    list("LKTSOPSHDER"),
+    list("XZKGEDSEIGU"),
 ]
 
 target = "POST"
@@ -22,40 +29,43 @@ for row in grid:
     row_index+=1
 
 #pass2_loci - search box around letter1, construct list of tuples
-for locus in pass1_loci:
+for locus1 in pass1_loci:
     pass2_loci = list()
-    l_one_c = locus[0]
-    l_one_r = locus[1]
-    if grid[l_one_c-1][l_one_r-1]==letter2:
-        pass2_loci.append((l_one_c-1, l_one_r-1))
-    if grid[l_one_c-1][l_one_r]==letter2:
-        pass2_loci.append((l_one_c-1,l_one_c))
-    if grid[l_one_c-1][l_one_r+1]==letter2:
-        pass2_loci.append((l_one_c-1,l_one_c+1))
-    if grid[l_one_c][l_one_r+1]==letter2:
-        pass2_loci.append((l_one_c,l_one_c+1))
-    if grid[l_one_c+1][l_one_r+1]==letter2:
-        pass2_loci.append((l_one_c+1,l_one_c+1))
-    if grid[l_one_c+1][l_one_r]==letter2:
-        pass2_loci.append((l_one_c+1,l_one_r))
-    if grid[l_one_c+1][l_one_r-1]==letter2:
-        pass2_loci.append((l_one_c+1,l_one_r-1))
-    if grid[l_one_c][l_one_r-1]==letter2:
-        pass2_loci.append((l_one_c,l_one_r-1))
+    L_one_c = locus1[0]
+    L_one_r = locus1[1]
+    #in the following note grid[r][c] -vs- pass2_loci((c,r)) transposed rc
+    if grid[L_one_r-1][L_one_c-1]==letter2:
+        pass2_loci.append((L_one_c-1, L_one_r-1))
+    if grid[L_one_r][L_one_c-1]==letter2:
+        pass2_loci.append((L_one_c-1,L_one_r))
+    if grid[L_one_r+1][L_one_c-1]==letter2:
+        pass2_loci.append((L_one_c-1,L_one_r+1))
+    if grid[L_one_r+1][L_one_c]==letter2:
+        pass2_loci.append((L_one_c,L_one_r+1))
+    if grid[L_one_r+1][L_one_c+1]==letter2:
+        pass2_loci.append((L_one_c+1,L_one_r+1))
+    if grid[L_one_r][L_one_c+1]==letter2:
+        pass2_loci.append((L_one_c+1,L_one_r))
+    if grid[L_one_r-1][L_one_c+1]==letter2:
+        pass2_loci.append((L_one_c+1,L_one_r-1))
+    if grid[L_one_r-1][L_one_c]==letter2:
+        pass2_loci.append((L_one_c,L_one_r-1))
     
-    for locus in pass2_loci:
-        vector = (pass2_loci(0)-l_one_c, pass2_loci(1)-l_one_r)
+    for locus2 in pass2_loci:
+        #vector index order r,c to match grid
+        vector = (locus2[1]-L_one_r, locus2[0]-L_one_c)
         #use vector to search for rest of target
         target_found = False
-        r = pass2_loci(1)
-        c = pass2_loci(0)
+        #start from locus of second letter
+        r = locus2[1]
+        c = locus2[0]
         for ch in target[2:]:
+            r+=vector[0]
+            c+=vector[1]            
             if grid[r][c]==ch:
                 target_found = True
             else:
                 target_found = False
                 break
-            
         if target_found:
             print("Found the target")
-
